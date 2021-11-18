68f99f74374875139da6dd61c03cbfd884fa5f1f
Hani <hani.lebguiret1@gmail.com>
2021-11-18 15:07:07 +0100

    correctif 9

--- 9/edit-transports-post.php
+++ 9/edit-transports-post.php
@@ -15,7 +15,7 @@ if (in_array('', $_POST)) {
     $terminusB = htmlspecialchars(trim($_POST['terminus_b']));
     $id = intval(htmlspecialchars(trim($_POST['id'])));
 }
-
+echo "ok1";
 $getId = explode('=', $_SERVER['HTTP_REFERER'])[1];
 // $altGetId = substr(strchr($_SERVER['HTTP_REFERER'], 'id'), 3);
 
@@ -28,15 +28,17 @@ if (!($getId == $id)) {
     header("Location:index.php?error=reqNotValid");
     exit();
 }
+echo "ok2";
 
 try {
     $editLine = 'UPDATE transports.lignesquatreCols SET nom_de_ligne=:nom_de_ligne, terminus_a=:terminus_a, terminus_b=:terminus_b WHERE id=:id';
     
     $reqEditLine = $connexion->prepare($editLine);
-    $reqEditLine->binValue(':nom_de_ligne', $nomdeligne, PDO::PARAM_STR);
-    $reqEditLine->binValue(':terminus_a', $terminusA, PDO::PARAM_STR);
-    $reqEditLine->binValue(':terminus_b', $terminusB, PDO::PARAM_STR);
-    $reqEditLine->binValue(':id', $id, PDO::PARAM_INT);
+    $reqEditLine->bindValue(':nom_de_ligne', $nomdeligne, PDO::PARAM_STR);
+    $reqEditLine->bindValue(':terminus_a', $terminusA, PDO::PARAM_STR);
+    $reqEditLine->bindValue(':terminus_b', $terminusB, PDO::PARAM_STR);
+    $reqEditLine->bindValue(':id', $id, PDO::PARAM_INT);
+echo "ok3";
     
     $reqEditLine->execute();
     header("Location:index.php?success=modif");
